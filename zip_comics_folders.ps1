1ffb3f96c1ae28d3ab876dfbae00df01b2569fcc
SabzTheDev <gsabz37@yahoo.fr>
2023-11-28 14:50:43 +0100

    Généraliation prise en charge des  mediatypes multiples  Mangas + Comics

--- zip_comics_folders.ps1
+++ zip_comics_folders.ps1
@@ -1,23 +1,16 @@
 ﻿# Définition du chemin de départ
 $rootPath = "G:\Graphic novels"
-$rootPathMangas = "G:\Graphic novels\Mangas"
+$mediaType = "Mangas"
+$rootPathTree = "${rootPath}\${mediaType}"
 $rootPathZip = "${rootPath}_zip"
 
 # Fonction pour vérifier la présence d'au moins deux images dans un répertoire
 function HasSomeImages($path) {
-    $imageExtensions = @("*.jpg", "*.png", "*.jpeg","*.gif")
-    $count = 0
-    foreach ($ext in $imageExtensions) {
-        $files = Get-ChildItem -LiteralPath $path -Filter $ext -File -ErrorAction SilentlyContinue
-        foreach ($file in $files) {
-            $count++
-            if ($count -ge 2) {
-                return $true
-            }
-        }
-    }
-    return $false
+    $imageExtensions = @("*.jpg", "*.png", "*.jpeg", "*.gif")
+    $count = (Get-ChildItem -LiteralPath $path -Include $imageExtensions -File -ErrorAction SilentlyContinue).Count
+    return $count -ge 2
 }
+
 
 # Fonction pour parcourir l'arborescence récursivement
 function TraverseDirectory($path) {  
@@ -32,11 +25,11 @@ function TraverseDirectory($path) {
         Write-Host "Parent : $parentPath" -ForegroundColor DarkMagenta
         $titleDirectory = Split-Path -Path $Path -Leaf
         $parentLeafDirectory = Split-Path -Path $parentPath -Leaf
-        if ($parentLeafDirectory.StartsWith("Mangas")) {
+        if ($parentLeafDirectory.StartsWith($mediaType)) {
            $rootPathZip = "${rootPath}\${parentLeafDirectory}_zip"  # Ex: "G:\Graphic novels\Mangas - Genre_zip"
            Write-Host "rootPathZip : $rootPathZip" -ForegroundColor Yellow
         } else { # il y un sous répertoire ex: G:\Graphic novels\Mangas\Author\Album
-            $rootPathZip = "${rootPathMangas}_zip"  
+            $rootPathZip = "${rootPathTree}_zip"  
             Write-Host "rootPathZip : $rootPathZip" -ForegroundColor DarkYellow
         }
         
@@ -47,11 +40,11 @@ function TraverseDirectory($path) {
         Write-Host "Zipping : $pathToCompress" -ForegroundColor Red
         Write-Host "into $destinationZip" -ForegroundColor Red
 
-        & "C:\Program Files (x86)\7-Zip\7z.exe" a -tzip $destinationZip $pathToCompress -r -mx0 -bso0
+       # & "C:\Program Files (x86)\7-Zip\7z.exe" a -tzip $destinationZip $pathToCompress -r -mx0 -bso0
     } else {
         Write-Host $path -ForegroundColor White
     }
 }
 
 # Lancement du script
-TraverseDirectory $rootPathMangas
+TraverseDirectory $rootPathTree
