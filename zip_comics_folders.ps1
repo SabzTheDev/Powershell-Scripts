8927d6b4caf24a7d1f6e4c61bac7ff55a4a90da2
SabzTheDev <gsabz37@yahoo.fr>
2023-11-29 20:34:36 +0100

    Variante de la précédente mais cette fois, on crée le répértoire marqué par _

--- zip_comics_folders.ps1
+++ zip_comics_folders.ps1
@@ -7,17 +7,24 @@ function ProcessTree([string]$_targetPath) {
     TraverseDirectory $rootPathTree
 }
 
+# Fonction pour vérifier la présence d'au moins deux images dans un répertoire
+function HasSomeImages($path) {
+    $imageExtensions = @("*.jpg", "*.png", "*.jpeg", "*.gif")
+    $count = (Get-ChildItem -LiteralPath $path -Include $imageExtensions -File -ErrorAction SilentlyContinue).Count
+    return $count -ge 2
+}
+
 # Fonction pour parcourir les sous répertoires de 1er niveau de l'arborescence.
 # Les répertoires de 1er niveau son zippés, les répertoires qui commencent par _ sont explorés récursivement.
 function TraverseDirectory([string]$path, [string]$optionalParentName='') {  
     $subFolders = Get-ChildItem -LiteralPath $path -Directory -ErrorAction SilentlyContinue
     foreach ($subFolder in $subFolders) {
         $subFolderPath = $subFolder.FullName
-        # Lorsqu'un réprtoire commence par _, ca veut dire qu'il faut l'explorer et intégrer le répertoire au path de destination.
+        # Lorsqu'un réprtoire commence par _, ca veut dire qu'il faut l'explorer et intégrer ce répertoire au path de destination.
         if ($subFolder.Name.StartsWith("_")) {
             $parentNameToPass = $subFolder.Name.Substring(1)  # Supprime le premier caractère "_"
-            TraverseDirectory $subFolderPath  $parentNameToPass
             Write-Host "Exploration du dossier: $($subFolder.Name)" -ForegroundColor Red
+            TraverseDirectory $subFolderPath  $parentNameToPass
             continue  # Passe à la prochaine itération de la boucle
         }
         $destinationPath = "${rootPathTree}_zip"  
@@ -25,7 +32,7 @@ function TraverseDirectory([string]$path, [string]$optionalParentName='') {
         if ($optionalParentName -eq '') {
             $destinationZip = "${destinationPath}\${subFolder}.zip"
         } else {
-            $destinationZip = "${destinationPath}\${optionalParentName}_${subFolder}.zip"
+            $destinationZip = "${destinationPath}\${optionalParentName}\${subFolder}.zip"
         }        
         Write-Host "Zipping : $destinationZip"
         Write-Host $subFolder -ForegroundColor DarkYellow -NoNewline
