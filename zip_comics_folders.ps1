0d26a74fb8d4f3fcc989e88fb224bdf1530b860e
SabzTheDev <gsabz37@yahoo.fr>
2023-11-29 19:07:13 +0100

    Vesion avec marquage du répertoire à explorer par _ et rappel du répertoire dans le nom du fichier zip.

--- zip_comics_folders.ps1
+++ zip_comics_folders.ps1
@@ -1,50 +1,39 @@
-﻿# Définition du chemin de départ
-$rootPath = "G:\Graphic novels"
-$mediaType = "Mangas"
-$rootPathTree = "${rootPath}\${mediaType}"
-$rootPathZip = "${rootPath}_zip"
-
-# Fonction pour vérifier la présence d'au moins deux images dans un répertoire
-function HasSomeImages($path) {
-    $imageExtensions = @("*.jpg", "*.png", "*.jpeg", "*.gif")
-    $count = (Get-ChildItem -LiteralPath $path -Include $imageExtensions -File -ErrorAction SilentlyContinue).Count
-    return $count -ge 2
+﻿
+# Execute le traitement sur l'arborescence.
+function ProcessTree([string]$_targetPath) {
+    $global:rootPath = Split-Path -Path $_targetPath -Parent
+    $global:mediaType = Split-Path -Path $_targetPath -Leaf
+    $rootPathTree = "${rootPath}\${mediaType}"
+    TraverseDirectory $rootPathTree
 }
 
-
-# Fonction pour parcourir l'arborescence récursivement
-function TraverseDirectory($path) {  
+# Fonction pour parcourir les sous répertoires de 1er niveau de l'arborescence.
+# Les répertoires de 1er niveau son zippés, les répertoires qui commencent par _ sont explorés récursivement.
+function TraverseDirectory([string]$path, [string]$optionalParentName='') {  
     $subFolders = Get-ChildItem -LiteralPath $path -Directory -ErrorAction SilentlyContinue
-    foreach ($folder in $subFolders) {
-        TraverseDirectory $folder.FullName
-    }
-    
-    if (HasSomeImages $path) {
-        Write-Host $path -ForegroundColor Gray
-        $parentPath = Split-Path -Path $path -Parent
-        Write-Host "Parent : $parentPath" -ForegroundColor DarkMagenta
-        $titleDirectory = Split-Path -Path $Path -Leaf
-        $parentLeafDirectory = Split-Path -Path $parentPath -Leaf
-        if ($parentLeafDirectory.StartsWith($mediaType)) {
-           $rootPathZip = "${rootPath}\${parentLeafDirectory}_zip"  # Ex: "G:\Graphic novels\Mangas - Genre_zip"
-           Write-Host "rootPathZip : $rootPathZip" -ForegroundColor Yellow
-        } else { # il y un sous répertoire ex: G:\Graphic novels\Mangas\Author\Album
-            $rootPathZip = "${rootPathTree}_zip"  
-            Write-Host "rootPathZip : $rootPathZip" -ForegroundColor DarkYellow
+    foreach ($subFolder in $subFolders) {
+        $subFolderPath = $subFolder.FullName
+        # Lorsqu'un réprtoire commence par _, ca veut dire qu'il faut l'explorer et intégrer le répertoire au path de destination.
+        if ($subFolder.Name.StartsWith("_")) {
+            $parentNameToPass = $subFolder.Name.Substring(1)  # Supprime le premier caractère "_"
+            TraverseDirectory $subFolderPath  $parentNameToPass
+            Write-Host "Exploration du dossier: $($subFolder.Name)" -ForegroundColor Red
+            continue  # Passe à la prochaine itération de la boucle
         }
-        
-        $pathToCompress = $path
-        $destinationZip = "${rootPathZip}\${titleDirectory}.zip"
-        # Write-Host $destinationZip
-        # Création de l'archive zip en utilisant 7-Zip
-        Write-Host "Zipping : $pathToCompress" -ForegroundColor Red
-        Write-Host "into $destinationZip" -ForegroundColor Red
-
-       # & "C:\Program Files (x86)\7-Zip\7z.exe" a -tzip $destinationZip $pathToCompress -r -mx0 -bso0
-    } else {
-        Write-Host $path -ForegroundColor White
+        $destinationPath = "${rootPathTree}_zip"  
+        $pathToCompress = $subFolderPath
+        if ($optionalParentName -eq '') {
+            $destinationZip = "${destinationPath}\${subFolder}.zip"
+        } else {
+            $destinationZip = "${destinationPath}\${optionalParentName}_${subFolder}.zip"
+        }        
+        Write-Host "Zipping : $destinationZip"
+        Write-Host $subFolder -ForegroundColor DarkYellow -NoNewline
+        Write-Host " zipped" -ForegroundColor Green
+        & "C:\Program Files (x86)\7-Zip\7z.exe" a -tzip $destinationZip $pathToCompress -r -mx0 -bso0
     }
 }
 
 # Lancement du script
-TraverseDirectory $rootPathTree
+#ProcessTree "G:\Graphic novels\Comics"
+ProcessTree "G:\Graphic novels\Mangas"
